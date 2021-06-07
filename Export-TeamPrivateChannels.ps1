009ecce30125d7129db755ebfb61c994af7edd01
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2021-06-07 14:04:11 +0100

    Added Teams Private Channels export

---
+++ Export-TeamPrivateChannels.ps1
@@ -0,0 +1,52 @@
+#Requires -Module MicrsoftTeams
+<#
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(
+        Mandatory,
+        HelpMessage = 'The location you would like the export csv to reside'
+    )][ValidateScript( {
+            if (!(Test-Path -Path $_))
+            {
+                throw "The folder $_ does not exist"
+            }
+            else
+            {
+                return $true
+            }
+        })]
+    [System.IO.DirectoryInfo]$OutputPath
+)
+$date = Get-Date -Format yyyyMMdd_hh_mm
+if (!$OutputPath.FullName.EndsWith([System.IO.Path]::DirectorySeparatorChar))
+{
+    $OutputPath = $OutputPath + [System.IO.Path]::DirectorySeparatorChar
+}
+$filepath = $OutputPath + "$date - Teams_PrivateChannels.csv"
+
+if (Test-Path $filepath -ErrorAction SilentlyContinue)
+{
+    Write-Error -Message "$filepath already exists - please delete the file and try again"
+    return
+}
+
+$teams = Get-Team
+$total = 0
+$privatechannels = @()
+$count = 0
+foreach ($team in $teams) {
+    $count ++
+    Write-Progress -Activity "Enumerating Private Teams" -Status "Checking Team $count of $($teams.count)" -PercentComplete (($count/($teams.count))*100)
+    $private = Get-TeamChannel -GroupId $team.groupid -MembershipType Private
+    $privateCount = $private.count
+    if ($privateCount -gt 0) {
+        $total = $total + $privateCount
+        $privatechannels += $private
+    }
+}
+$total
+
+$privatechannels | Export-CSV $filepath -NoClobber -NoTypeInformation
