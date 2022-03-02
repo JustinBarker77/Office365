79aa98503ad93819d2f8e3bc5969e1009db38908
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-03-02 16:09:03 +0000

    New: Get and Export Mailbox sizes
    Co-Authored-By: LukeAllinson <69592809+LukeAllinson@users.noreply.github.com>

--- Module/M365Reporting/M365Reporting.psm1
+++ Module/M365Reporting/M365Reporting.psm1
@@ -1,16 +1,16 @@
 if ($PSEdition -eq 'Core' -and $IsWindows)
 {
-    Import-WinModule 'MSOnline', 'Microsoft.Online.SharePoint.PowerShell' -DisableNameChecking
+    Import-Module 'MSOnline', 'Microsoft.Online.SharePoint.PowerShell' -DisableNameChecking -UseWindowsPowerShell
 }
 
 foreach ($directory in @('Private', 'Public'))
 {
     if ($PSEdition -eq 'Core' -and -not $IsWindows)
     {
-        Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" -Exclude 'Get-OneDriveUsageReport.ps1', 'Export-OneDriveUsageReport.ps1' | ForEach-Object { . $_.FullName }
+        Get-ChildItem -Path $(Join-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath $directory) -ChildPath '*.ps1') -Exclude 'Get-OneDriveUsageReport.ps1', 'Export-OneDriveUsageReport.ps1' | ForEach-Object { . $_.FullName }
     }
     else
     {
-        Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1" | ForEach-Object { . $_.FullName }
+        Get-ChildItem -Path $(Join-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath $directory) -ChildPath '*.ps1') | ForEach-Object { . $_.FullName }
     }
 }
