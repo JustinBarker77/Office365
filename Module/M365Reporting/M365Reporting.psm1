125881aeb10a3f36f0acbd351d276861771f71a2
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-03-03 16:39:02 +0000

    ignores msol for PSCore not on Windows

--- Module/M365Reporting/M365Reporting.psm1
+++ Module/M365Reporting/M365Reporting.psm1
@@ -7,7 +7,7 @@ foreach ($directory in @('Private', 'Public'))
 {
     if ($PSEdition -eq 'Core' -and -not $IsWindows)
     {
-        Get-ChildItem -Path $(Join-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath $directory) -ChildPath '*.ps1') -Exclude 'Get-OneDriveUsageReport.ps1', 'Export-OneDriveUsageReport.ps1' | ForEach-Object { . $_.FullName }
+        Get-ChildItem -Path $(Join-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath $directory) -ChildPath '*.ps1') -Exclude 'Export-MSOLUserLicenceBreakdown.ps1','Get-OneDriveUsageReport.ps1', 'Export-OneDriveUsageReport.ps1' | ForEach-Object { . $_.FullName }
     }
     else
     {
