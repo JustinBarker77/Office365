f4b7b29346b1b4073e5805f2ddd7d398f0596e4d
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-03-08 17:57:58 +0000

    Fixed: Build script

--- build/build.ps1
+++ build/build.ps1
@@ -5,7 +5,8 @@ $VerbosePreference = 'SilentlyContinue'
 #Gets Cloud Version in case of major change
 [version]$CloudVersion = (Find-Module M365Reporting).Version
 #Gets Manifest Version in case of major change
-[version]$LocalVersion = (Import-PowerShellDataFile '.\M365Reporting\M365Reporting.psd1').ModuleVersion
+$manifest = Import-PowerShellDataFile '.\M365Reporting\M365Reporting.psd1'
+[version]$LocalVersion = $manifest.ModuleVersion
 
 #Checks if Major Version Change to Module Manifest
 if ($LocalVersion -gt $CloudVersion)
@@ -17,9 +18,10 @@ else
     [version]$NewVersion = "{0}.{1}.{2}" -f $CloudVersion.Major, $CloudVersion.Minor, ($CloudVersion.Build + 1)
 }
 
-"Cloud Version is $($CloudVersion)"
-"Local Version is $($LocalVersion)"
-"New Version is $($NewVersion)"
+foreach ($module in $manifest.RequiredModules.ModuleName)
+{
+    Install-Module -Name $module -Confirm:$false -Force -AllowClobber
+}
 
 #Updates Module Manifest ready for publish
 Update-ModuleManifest -Path .\M365Reporting\M365Reporting.psd1 -ModuleVersion $NewVersion -ReleaseNotes $($env:COMMIT_MESSAGE)
